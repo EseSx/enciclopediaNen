c4fc6a7628c4166405e2e8289a6c08e2d5691f2c
EseSx <santiagoeseiza10@gmail.com>
2025-05-17 23:25:08 -0300

    Añadi la documentacion del proyecto

--- paginaProfesional.js
+++ paginaProfesional.js
@@ -66,157 +66,253 @@
 // })
 
 // Botones del CRU
-const botonIngresar = document.getElementById("botonIngresar")
-const botonModificar = document.getElementById("botonModificar")
-const botonEliminar = document.getElementById("botonEliminar")
+const botonIngresar = document.getElementById("botonIngresar");
+const botonModificar = document.getElementById("botonModificar");
+const botonEliminar = document.getElementById("botonEliminar");
 
 // Formulario de personajes
 // contenedores
-const contenedorForm = document.getElementById("contenedorForm")
-const contenedorPHOA = document.getElementById("contenedorPHOA")
+const contenedorForm = document.getElementById("contenedorForm");
+const contenedorPHOA = document.getElementById("contenedorPHOA");
+
 // PHOA |"PHOA" Es la eleccion Personaje, Habilidad, Organizacion/Afiliacion que existe en cada formulario|
-const PHOA = document.getElementById("PHOA")
-const Personaje = document.getElementById("Personaje")
-const Habilidad = document.getElementById("Habilidad")
-const OrganizacionAfiliacion = document.getElementById("OrganizacionAfiliacion")
+const PHOA = document.getElementById("PHOA");
+const Personaje = document.getElementById("Personaje");
+const Habilidad = document.getElementById("Habilidad");
+const OrganizacionAfiliacion = document.getElementById(
+  "OrganizacionAfiliacion"
+);
+
 // Pedido general de datos
-const pedidoDatos = document.getElementById("pedidoDatos")
-const inputPedidoDatos = document.getElementById("inputPedidoDatos")
-const botonCerrar = document.getElementById("botonCerrar")
-const botonSiguiente = document.getElementById("botonSiguiente")
+const pedidoDatos = document.getElementById("pedidoDatos");
+const inputPedidoDatos = document.getElementById("inputPedidoDatos");
+const botonCerrar = document.getElementById("botonCerrar");
+const botonSiguiente = document.getElementById("botonSiguiente");
 
 // Listas de datos a ingresar por cada form
 const titulosDatosIngresar = [
-    "Ingrese el nombre completo del personaje",
-    "Ingrese la descripcion del personaje",
-    "Ingrese la URL de una imagen para representar al personaje",
-    "Ingrese las afiliaciones del personaje (Tome en cuenta que debera separarlas por puntos)",
-    "Ingrese las habilidades nen del personaje (Tome en cuenta que debera separarlas por puntos)",
-    "Ingrese el color representativo del personaje"
-]
+  "Ingrese el nombre completo del personaje",
+  "Ingrese la descripcion del personaje",
+  "Ingrese la URL de una imagen para representar al personaje",
+  "Ingrese las afiliaciones del personaje (Tome en cuenta que el espaciado debe darse en foramto de espacio comun, puntos o guiones bajos)",
+  "Ingrese las habilidades nen del personaje (Tome en cuenta las mismas especificaciones en el espaciado)",
+  "Ingrese el color representativo del personaje",
+];
 
 // Datos a enviar a la api
-let nombreJson, descripcionJson, urlJson, afiliacionJson, habilidadesJson, colorJson
+let nombreJson,
+  descripcionJson,
+  urlJson,
+  afiliacionJson,
+  habilidadesJson,
+  colorJson;
+
 // Indice de las listas de datos previamente nombradas
-let indicePedidos = 0
+let indicePedidos = 0;
+
 // Contenedor de los formularios
-let contenedorDesplegable
+let contenedorDesplegable;
 
 function manejarclick() {
-    // Guardo cada dato y reseteo el input despues de cada uso
+  // Guardo cada dato y reseteo el input despues de cada uso
+  switch (indicePedidos) {
+    case 0:
+      nombreJson = inputPedidoDatos.value;
+      inputPedidoDatos.value = "";
+      break;
+    case 1:
+      descripcionJson = inputPedidoDatos.value;
+      inputPedidoDatos.value = "";
+      break;
+    case 2:
+      urlJson = inputPedidoDatos.value;
+      inputPedidoDatos.value = "";
+      break;
+    case 3:
+      afiliacionJson = inputPedidoDatos.value;
+      inputPedidoDatos.value = "";
+      break;
+    case 4:
+      habilidadesJson = inputPedidoDatos.value;
+      inputPedidoDatos.value = "";
+      break;
+    case 5:
+      colorJson = inputPedidoDatos.value;
+      break;
+  }
+
+  // La logica para el envio de datos a la api
+  // |IMPORTANTE| Falta conectar a la api
+  if (botonSiguiente.innerHTML === "ENVIAR") {
+    indicePedidos = 0;
+    document.body.removeChild(contenedorDesplegable);
+    const data = {
+      nombre: nombreJson,
+      descripcion: descripcionJson,
+      url: urlJson,
+      afiliacion: afiliacionJson,
+      habilidades: habilidadesJson,
+      color: colorJson,
+    };
+    console.log(data);
+  }
+
+  // Asigna el tipo de dato que se va a ingresar en cada caso y aumenta el indice
+  if (indicePedidos < titulosDatosIngresar.length - 1) {
+    indicePedidos++;
+    pedidoDatos.innerHTML = titulosDatosIngresar[indicePedidos];
     switch (indicePedidos) {
-        case 0:
-            nombreJson = inputPedidoDatos.value
-            inputPedidoDatos.value = ""
-            break
-        case 1:
-            descripcionJson = inputPedidoDatos.value
-            inputPedidoDatos.value = ""
-            break
-        case 2:
-            urlJson = inputPedidoDatos.value
-            inputPedidoDatos.value = ""
-            break
-        case 3:
-            afiliacionJson = inputPedidoDatos.value
-            inputPedidoDatos.value = ""
-            break
-        case 4:
-            habilidadesJson = inputPedidoDatos.value
-            inputPedidoDatos.value = ""
-            break
-        case 5:
-            colorJson = inputPedidoDatos.value
-            break
+      case 0:
+      case 1:
+      case 3:
+      case 4:
+        inputPedidoDatos.type = "text";
+        break;
+      case 2:
+        inputPedidoDatos.type = "url";
+        break;
+      case 5:
+        inputPedidoDatos.type = "color";
+        break;
     }
+  }
 
-    // La logica par ael envio de datos a la api
-    // |IMPORTANTE| Falta conectar a la api
-    if (botonSiguiente.innerHTML === "ENVIAR") {
-        indicePedidos = 0
-        document.body.removeChild(contenedorDesplegable)
-        const data = {
-            nombre: nombreJson,
-            descripcion: descripcionJson,
-            url: urlJson,
-            afiliacion: afiliacionJson,
-            habilidades: habilidadesJson,
-            color: colorJson
-        }
-        console.log(data)
-    }
+  // Cambia el texto de siguiente en el caso de que no hayan mas opciones
+  if (indicePedidos === titulosDatosIngresar.length - 1) {
+    botonSiguiente.innerHTML = "ENVIAR";
+  } else {
+    botonSiguiente.innerHTML = "SIGUIENTE";
+  }
 
-    // Asigna el tipo de dato que se va a ingresar en cada caso
-    if (indicePedidos < titulosDatosIngresar.length - 1) {
-        indicePedidos++
-        pedidoDatos.innerHTML = titulosDatosIngresar[indicePedidos]
-        switch (indicePedidos) {
-            case 0:
-            case 1:
-            case 3:
-            case 4:
-                inputPedidoDatos.type = "text"
-                break
-            case 2:
-                inputPedidoDatos.type = "url"
-                break
-            case 5:
-                inputPedidoDatos.type = "color"
-                break
-        }
-    }
-
-    // Cambia el texto de siguiente en el caso de que no hayan mas opciones
-    if (indicePedidos === titulosDatosIngresar.length - 1) {
-        botonSiguiente.innerHTML = "ENVIAR"
-    }
+  if (indicePedidos !== 0) {
+    botonCerrar.innerHTML = "ATRAS";
+  } else {
+    botonCerrar.innerHTML = "CERRAR";
+  }
 }
 
 // Utilidad del boton "Ingresar"
 botonIngresar.addEventListener("click", function () {
+  // Organizacion de datos para mostrar los formularios en pantalla
+  contenedorDesplegable = document.createElement("div");
+  contenedorDesplegable.classList.add("contenedorDesplegable");
+  const div = document.createElement("div");
+  div.classList.add("divDesplegable");
+
+  document.body.appendChild(contenedorDesplegable);
+  contenedorDesplegable.appendChild(div);
+  div.appendChild(contenedorPHOA);
+  contenedorPHOA.style.display = "flex";
+
+  // Asignacion de textos a los elementos del PHOA
+  PHOA.innerHTML = "Vamos a conjurar nuevos datos";
+  Personaje.innerHTML = "Conjurar personaje";
+  Habilidad.innerHTML = "Conjurar habilidad";
+  OrganizacionAfiliacion.innerHTML = "Conjurar organizacion/afiliacion";
+
+  // Si se selecciona "Personaje"
+  Personaje.addEventListener("click", function () {
+    // Borro el contenedor previo
+    document.body.removeChild(contenedorDesplegable);
+
+    // Reseteo los valores a 0 cada vez que se vuelve a seleccionar el formulario
+    indicePedidos = 0;
+    nombreJson =
+      descripcionJson =
+      urlJson =
+      afiliacionJson =
+      habilidadesJson =
+      colorJson =
+      inputPedidoDatos.value =
+        "";
 
     // Organizacion de datos para mostrar los formularios en pantalla
-    contenedorDesplegable = document.createElement("div")
-    contenedorDesplegable.classList.add("contenedorDesplegable")
-    const div = document.createElement("div")
-    div.classList.add("divDesplegable")
+    contenedorDesplegable = document.createElement("div");
+    contenedorDesplegable.classList.add("contenedorDesplegable");
+    const div = document.createElement("div");
+    div.classList.add("divDesplegable");
 
-    document.body.appendChild(contenedorDesplegable)
-    contenedorDesplegable.appendChild(div)
-    div.appendChild(contenedorPHOA)
-    contenedorPHOA.style.display = "flex"
+    // Le doy visibilidad al Form
+    document.body.appendChild(contenedorDesplegable);
+    contenedorDesplegable.appendChild(div);
+    div.appendChild(contenedorForm);
+    contenedorForm.style.display = "flex";
 
-    // Asignacion de textos a los elementos del PHOA
-    PHOA.innerHTML = "Vamos a conjurar nuevos datos"
-    Personaje.innerHTML = "Conjurar personaje"
-    Habilidad.innerHTML = "Conjurar habilidad"
-    OrganizacionAfiliacion.innerHTML = "Conjurar organizacion/afiliacion"
+    // Asigno lo que se va a mostrar el primera instancia, y borro los datos que puedan quedar en el input
+    pedidoDatos.innerHTML = titulosDatosIngresar[indicePedidos];
+    botonSiguiente.innerHTML = "SIGUIENTE";
+    inputPedidoDatos.value = "";
+    inputPedidoDatos.type = "text";
 
-    // Si se selecciona "Personaje"
-    Personaje.addEventListener("click", function () {
-        // Reseteo los valores a 0 cada vez que se vuelve a seleccionar el formulario
-        indicePedidos = 0
-        nombreJson = descripcionJson = urlJson = afiliacionJson = habilidadesJson = colorJson = inputPedidoDatos.value = ""
+    botonSiguiente.removeEventListener("click", manejarclick);
+    botonSiguiente.addEventListener("click", manejarclick);
 
-        // Organizacion de datos para mostrar los formularios en pantalla
-        contenedorDesplegable = document.createElement("div")
-        contenedorDesplegable.classList.add("contenedorDesplegable")
-        const div = document.createElement("div")
-        div.classList.add("divDesplegable")
+    botonCerrar.addEventListener("click", function () {
+      // Borro el dato anterior y reseteo el input
+      switch (indicePedidos) {
+        case 0:
+          nombreJson = "";
+          inputPedidoDatos.value = "";
+          break;
+        case 1:
+          descripcionJson = "";
+          inputPedidoDatos.value = "";
+          break;
+        case 2:
+          urlJson = "";
+          inputPedidoDatos.value = "";
+          break;
+        case 3:
+          afiliacionJson = "";
+          inputPedidoDatos.value = "";
+          break;
+        case 4:
+          habilidadesJson = "";
+          inputPedidoDatos.value = "";
+          break;
+        case 5:
+          colorJson = "";
+          break;
+      }
 
-        // Le doy visibilidad al Form
-        document.body.appendChild(contenedorDesplegable)
-        contenedorDesplegable.appendChild(div)
-        div.appendChild(contenedorForm)
-        contenedorForm.style.display = "block"
+      // La logica para cerrar el contenedor
+      if (botonCerrar.innerHTML === "CERRAR") {
+        indicePedidos = 0;
+        document.body.removeChild(contenedorDesplegable);
+      }
 
-        // Asigno lo que se va a mostrar el primera instancia, y borro los datos que puedan quedar en el input
-        pedidoDatos.innerHTML = titulosDatosIngresar[indicePedidos]
-        botonSiguiente.innerHTML = "SIGUIENTE"
-        inputPedidoDatos.value = ""
-        inputPedidoDatos.type = "text"
+      // Asigna el tipo de dato que se va a ingresar en cada caso y reduzco el indice
+      if (indicePedidos > 0) {
+        indicePedidos = indicePedidos - 1;
+        pedidoDatos.innerHTML = titulosDatosIngresar[indicePedidos];
+        switch (indicePedidos) {
+          case 0:
+          case 1:
+          case 3:
+          case 4:
+            inputPedidoDatos.type = "text";
+            break;
+          case 2:
+            inputPedidoDatos.type = "url";
+            break;
+          case 5:
+            inputPedidoDatos.type = "color";
+            break;
+        }
+      }
 
-        botonSiguiente.removeEventListener("click", manejarclick)
-        botonSiguiente.addEventListener("click", manejarclick)
-    })
-})
+      // Cambia el texto de siguiente en el caso de que no hayan mas opciones
+      if (indicePedidos === titulosDatosIngresar.length - 1) {
+        botonSiguiente.innerHTML = "ENVIAR";
+      } else {
+        botonSiguiente.innerHTML = "SIGUIENTE";
+      }
+
+      if (indicePedidos !== 0) {
+        botonCerrar.innerHTML = "ATRAS";
+      } else {
+        botonCerrar.innerHTML = "CERRAR";
+      }
+    });
+  });
+});
